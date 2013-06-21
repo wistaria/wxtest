ff863b825ae12b7969a04e7f2267bbc151fb29ad
wistaria <synge.todo@gmail.com>
2013-06-21 13:14:17 +0200

    Updated 4-3

--- tokoi/example-4-3.py
+++ tokoi/example-4-3.py
@@ -9,26 +9,31 @@ class GLFrame(wx.Frame):
     """
     def __init__(self, parent, id, title, pos=wx.DefaultPosition, size=wx.DefaultSize,
                  style=wx.DEFAULT_FRAME_STYLE, name='frame'):
+        print "GLFrame.__init__"
         super(GLFrame, self).__init__(parent, id, title, pos, size, style, name)
         self.canvas = glcanvas.GLCanvas(self)
-        self.context = glcanvas.GLContext(self.canvas)
         self.initialized = False
 
         self.Bind(wx.EVT_PAINT, self.OnPaint)
 
     def OnPaint(self, event):
-        self.canvas.SetCurrent(self.context)
+        print "GLFrame.OnPaint"
         if not self.initialized:
             self.InitGL()
             self.initialized = True
         self.OnDraw()
         
     def InitGL(self):
+        print "GLFrame.InitGL"
+        self.context = glcanvas.GLContext(self.canvas)
+        self.canvas.SetCurrent(self.context)
         glClearColor(0, 0, 0, 1)
         width, height = self.GetClientSize()
         glViewport(0, 0, width, height)
 
     def OnDraw(self):
+        print "GLFrame.OnDraw"
+        self.canvas.SetCurrent(self.context)
         glClear(GL_COLOR_BUFFER_BIT)
         glBegin(GL_LINE_LOOP)
         glVertex2d(-0.9, -0.9)
