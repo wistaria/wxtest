e9454032fb5f89aab9d993735848bba8ec2ee34c
wistaria <synge.todo@gmail.com>
2013-06-21 09:47:08 +0200

    Introduced GLFrame class

--- tokoi/example-4-1.py
+++ tokoi/example-4-1.py
@@ -1,8 +1,17 @@
 import sys
 import wx
+from wx import glcanvas
+
+class GLFrame(wx.Frame):
+    """A simple class for using OpenGL with wxPython.
+    """
+    def __init__(self, parent, id, title, pos=wx.DefaultPosition, size=wx.DefaultSize,
+                 style=wx.DEFAULT_FRAME_STYLE, name='frame'):
+        super(GLFrame, self).__init__(parent, id, title, pos, size, style, name)
+        self.canvas = glcanvas.GLCanvas(self)
 
 app = wx.App()
-frame = wx.Frame(None, -1, sys.argv[0])
+frame = GLFrame(None, -1, sys.argv[0])
 frame.Show()
 
 app.MainLoop()
