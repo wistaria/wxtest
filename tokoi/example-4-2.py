856da47ec8046e4772ce720d0298151fb8f7d37d
wistaria <synge.todo@gmail.com>
2013-06-21 12:50:17 +0200

    Added example-4.2

--- tokoi/example-4-2.py
+++ tokoi/example-4-2.py
@@ -1,6 +1,7 @@
 import sys
 import wx
 from wx import glcanvas
+from OpenGL.GL import *
 
 class GLFrame(wx.Frame):
     """A simple class for using OpenGL with wxPython.
@@ -9,6 +10,25 @@ class GLFrame(wx.Frame):
                  style=wx.DEFAULT_FRAME_STYLE, name='frame'):
         super(GLFrame, self).__init__(parent, id, title, pos, size, style, name)
         self.canvas = glcanvas.GLCanvas(self)
+        self.context = glcanvas.GLContext(self.canvas)
+        self.initialized = False
+
+        self.Bind(wx.EVT_PAINT, self.OnPaint)
+
+    def OnPaint(self, event):
+        self.canvas.SetCurrent(self.context)
+        if not self.initialized:
+            self.InitGL()
+            self.initialized = True
+        self.OnDraw()
+        
+    def InitGL(self):
+        glClearColor(1, 1, 1, 1)
+
+    def OnDraw(self):
+        glClear(GL_COLOR_BUFFER_BIT)
+        glFlush()
+        self.canvas.SwapBuffers()
 
 if __name__ == '__main__':
     app = wx.App()
