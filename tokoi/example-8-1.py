7bf918e60a6a6321205e39b1719b06fc7d59c2de
wistaria <synge.todo@gmail.com>
2013-06-22 16:02:05 +0900

    Added 8-1

---
+++ tokoi/example-8-1.py
@@ -0,0 +1,46 @@
+import sys
+import wx
+from wx import glcanvas
+from OpenGL.GL import *
+
+class MyCanvas(glcanvas.GLCanvas):
+    def __init__(self, parent):
+        super(MyCanvas, self).__init__(parent, -1, attribList=[])
+        self.context = glcanvas.GLContext(self)
+        self.initialized = False
+        self.Bind(wx.EVT_SIZE, self.OnSize)
+        self.Bind(wx.EVT_PAINT, self.OnPaint)
+
+    def InitGL(self):
+        glClearColor(0, 0, 0, 1)
+
+    def OnSize(self, event):
+        w, h = self.GetClientSize()
+        glViewport(0, 0, w, h)
+        
+    def OnPaint(self, event):
+        self.SetCurrent(self.context)
+        if not self.initialized:
+            self.InitGL()
+            self.initialized = True
+        glClear(GL_COLOR_BUFFER_BIT)
+        glRotated(25.0, 0.0, 1.0, 0.0)
+        glBegin(GL_POLYGON)
+        glColor3d(1.0, 0.0, 0.0) # red
+        glVertex2d(-0.9, -0.9)
+        glColor3d(0.0, 1.0, 0.0) # green
+        glVertex2d(0.9, -0.9)
+        glColor3d(0.0, 0.0, 1.0) # blue
+        glVertex2d(0.9, 0.9)
+        glColor3d(1.0, 1.0, 0.0) # yellow
+        glVertex2d(-0.9, 0.9)
+        glEnd()
+        glFlush()
+
+if __name__ == '__main__':
+    app = wx.App()
+    frame = wx.Frame(None, -1, sys.argv[0])
+    canvas = MyCanvas(frame)
+    frame.Show()
+    app.MainLoop()
+    app.Destroy()
