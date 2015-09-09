0144e280d720859f7af4de78d93213ec7c98ecba
Synge Todo <wistaria@phys.s.u-tokyo.ac.jp>
2015-09-09 14:41:09 +0900

    Added 7-2

--- tokoi/example-7-2.py
+++ tokoi/example-7-2.py
@@ -8,16 +8,14 @@ class MyCanvas(glcanvas.GLCanvas):
         super(MyCanvas, self).__init__(parent, -1, attribList=[])
         self.context = glcanvas.GLContext(self)
         self.initialized = False
+        self.point = []
+        self.rubberband = False
         self.Bind(wx.EVT_SIZE, self.OnSize)
         self.Bind(wx.EVT_PAINT, self.OnPaint)
-        self.Bind(wx.EVT_LEFT_DOWN, self.OnMouseLeftDown)
-        self.Bind(wx.EVT_LEFT_UP, self.OnMouseLeftUp)
-        self.Bind(wx.EVT_MOTION, self.OnMouseMotion)
-        self.lines = []
-        self.rubberband = False
+        self.Bind(wx.EVT_MOUSE_EVENTS, self.OnMouse)
 
     def InitGL(self):
-        glClearColor(1, 1, 1, 1)
+        glClearColor(1.0, 1.0, 1.0, 1.0)
 
     def OnSize(self, event):
         w, h = self.GetClientSize()
@@ -31,49 +29,41 @@ class MyCanvas(glcanvas.GLCanvas):
             self.InitGL()
             self.initialized = True
         glClear(GL_COLOR_BUFFER_BIT)
-        glBegin(GL_LINES)
-        for l in self.lines:
-            glVertex2iv(l[0])
-            glVertex2iv(l[1])
-        glEnd()
+        glColor3d(0.0, 0.0, 0.0);
+        glBegin(GL_LINES);
+        for p in self.point:
+            glVertex2iv(p);
+        glEnd();
         glFlush()
+        self.OnSize(event)
 
-    def OnMouseLeftDown(self, event):
-        x, y = event.GetPosition()
-        self.start = [x, y]
-
-    def OnMouseLeftUp(self, event):
-        x, y = event.GetPosition()
-        stop = [x, y]
-        self.lines.append([self.start, stop])
-        glColor3d(0.0, 0.0, 0.0)
-        glBegin(GL_LINES)
-        glVertex2iv(self.start)
-        glVertex2iv(stop)
-        glEnd()
-        glFlush()
-        self.rubberband = False
-
-    def OnMouseMotion(self, event):
-        if event.Dragging() and event.LeftIsDown():
+    def OnMouse(self, event):
+        if (event.IsButton() and event.GetButton() == wx.MOUSE_BTN_LEFT):
+            x, y = event.GetPosition()
+            self.point.append([x, y])
+            if (event.ButtonUp()):
+                glColor3d(0.0, 0.0, 0.0)
+                glBegin(GL_LINES)
+                glVertex2iv(self.point[-2])
+                glVertex2iv(self.point[-1])
+                glEnd()
+                glFlush()
+                self.rubberband = False
+        if (event.Dragging()):
+            x, y = event.GetPosition()
             glEnable(GL_COLOR_LOGIC_OP)
             glLogicOp(GL_INVERT)
-
             glBegin(GL_LINES)
             if (self.rubberband):
-                glVertex2iv(self.start)
-                glVertex2iv(self.stop)
-
-            x, y = event.GetPosition()
-            self.stop = [x, y]
-            glVertex2iv(self.start)
-            glVertex2iv(self.stop)
+                glVertex2iv(self.point[-1])
+                glVertex2iv(self.savepoint)
+            glVertex2iv(self.point[-1])
+            glVertex2iv([x, y])
             glEnd()
             glFlush()
-
             glLogicOp(GL_COPY)
             glDisable(GL_COLOR_LOGIC_OP)
-
+            self.savepoint = [x, y]
             self.rubberband = True
 
 if __name__ == '__main__':
